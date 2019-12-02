97d97a4ffb457decfe1b217c30ff5741e372caee
Rufai Damilola Ibraheem <rufaidamilolaibraheem1995@gmail.com>
2019-12-02 19:48:55 +0200

    Initial Commit

---
+++ processmaker/shared/compiled/xmlform/7897743125d6962a4d7b771059968175/6993919325de0f1ef2d0bd1039699809.755ec23b0451d33c51dcb5ac97e5067e.js
@@ -0,0 +1,28 @@
+    var form_YXB0cG8yZW5aMkdpcVpXanJtdHIwMlhSYXNWbm8yR2phV21wcW1tZXJHcG9xR3FtWnB4aHBXT2xhWlRWcEplZTJwdHIxbUhQbDVSZ24ybXBiV21vcEdxYjdhS2w___;
+  var object_7897743125d6962a4d7b771059968175_6993919325de0f1ef2d0bd1039699809;
+
+  if (typeof(__aObjects__) == "undefined") {
+      var __aObjects__ = [];
+  }
+
+    function loadForm_YXB0cG8yZW5aMkdpcVpXanJtdHIwMlhSYXNWbm8yR2phV21wcW1tZXJHcG9xR3FtWnB4aHBXT2xhWlRWcEplZTJwdHIxbUhQbDVSZ24ybXBiV21vcEdxYjdhS2w___(ajaxServer)
+    {
+      swSubmitValidateForm = 1;
+      var i = 0;
+
+      if (typeof(G_Form) == "undefined") {
+          return alert("form.js was not loaded");
+      }
+
+      form_YXB0cG8yZW5aMkdpcVpXanJtdHIwMlhSYXNWbm8yR2phV21wcW1tZXJHcG9xR3FtWnB4aHBXT2xhWlRWcEplZTJwdHIxbUhQbDVSZ24ybXBiV21vcEdxYjdhS2w___=new G_Form(document.getElementById('YXB0cG8yZW5aMkdpcVpXanJtdHIwMlhSYXNWbm8yR2phV21wcW1tZXJHcG9xR3FtWnB4aHBXT2xhWlRWcEplZTJwdHIxbUhQbDVSZ24ybXBiV21vcEdxYjdhS2w___'),'YXB0cG8yZW5aMkdpcVpXanJtdHIwMlhSYXNWbm8yR2phV21wcW1tZXJHcG9xR3FtWnB4aHBXT2xhWlRWcEplZTJwdHIxbUhQbDVSZ24ybXBiV21vcEdxYjdhS2w___');
+      object_7897743125d6962a4d7b771059968175_6993919325de0f1ef2d0bd1039699809 = form_YXB0cG8yZW5aMkdpcVpXanJtdHIwMlhSYXNWbm8yR2phV21wcW1tZXJHcG9xR3FtWnB4aHBXT2xhWlRWcEplZTJwdHIxbUhQbDVSZ24ybXBiV21vcEdxYjdhS2w___;
+      __aObjects__.push(object_7897743125d6962a4d7b771059968175_6993919325de0f1ef2d0bd1039699809);
+      var myForm=form_YXB0cG8yZW5aMkdpcVpXanJtdHIwMlhSYXNWbm8yR2phV21wcW1tZXJHcG9xR3FtWnB4aHBXT2xhWlRWcEplZTJwdHIxbUhQbDVSZ24ybXBiV21vcEdxYjdhS2w___;
+      if (myForm.aElements===undefined) alert("7897743125d6962a4d7b771059968175_6993919325de0f1ef2d0bd1039699809");
+      myForm.ajaxServer = ajaxServer;
+
+        //
+        
+                  }
+
+    
